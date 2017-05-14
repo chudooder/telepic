c35619634269d78cd3a95adeb1665ad5dbebe112
chudooder <codeshawnachu@gmail.com>
2017-05-14 14:12:31 -0500

    made canvas take up height of page

--- js/index.js
+++ js/index.js
@@ -389,37 +389,109 @@
 },{}],2:[function(require,module,exports){
 var Sketchpad = require('responsive-sketchpad');
 
-// Initialize sketchpad
-var el = $('#sketchpad')[0];
-var pad = new Sketchpad(el, {
-    line: {
-        color: '#000000',
-        size: 5
-    }
+var padWidth;
+var padHeight;
+var curPad;
+
+
+$(window).on("load", function() {
+    // Initialize sketchpad
+    var el = $('#game-area')[0];
+    padWidth = $('#game-area').width();
+    padHeight = $(window).height() * 0.7;
+    console.log(padWidth + " " + padHeight);
+    appendTextArea();
+    appendSketchpad();
 });
 
-$(document).ready(function() {
+function appendSketchpad() {
+    var gameArea = $('#game-area');
+    var row = $('<div class="row"></div>');
+    var col1 = $('<div class="two-thirds column"></div>');
+    var panel = $('<div class="drawpanel" id="sketchpad"></div>');
 
-});
+    panel.appendTo(col1);
 
-$('#clear-btn').click(function() {
-    pad.clear();
-});
+    var buttons = $('<div class="one-third column">\
+          <div class="row">\
+            <div class="col-sm-1">\
+              <div class="btn btn-lg color-btn" id="c-black-btn">Blk</div>\
+            </div>\
+            <div class="col-sm-1">\
+              <div class="btn btn-lg color-btn" id="c-red-btn">Red</div>\
+            </div>\
+            <div class="col-sm-1">\
+              <div class="btn btn-lg color-btn" id="c-green-btn">Grn</div>\
+            </div>\
+            <div class="col-sm-1">\
+              <div class="btn btn-lg color-btn" id="c-blue-btn">Blu</div>\
+            </div>\
+            <div class="col-sm-4">\
+              <div class="btn btn-lg btn-block btn-danger action-btn" id="clear-btn">Clear</div>\
+            </div>\
+            <div class="col-sm-4">\
+              <div class="btn btn-lg btn-block btn-success action-btn" id="submit-btn">Submit</div>\
+            </div>\
+          </div>\
+        </div>');
 
-$('#c-black-btn').click(function() {
-    pad.setLineColor('#000000');
-});
+    curPad = new Sketchpad(panel[0], {
+        line: {
+            color: '#000000',
+            size: 5
+        },
+        width: padWidth,
+        height: padHeight
+    });
 
-$('#c-red-btn').click(function() {
-    pad.setLineColor('#FF0000');
-});
+    col1.appendTo(row);
+    buttons.appendTo(row);
 
-$('#c-green-btn').click(function() {
-    pad.setLineColor('#00FF00');
-});
+    row.appendTo(gameArea);
 
-$('#c-blue-btn').click(function() {
-    pad.setLineColor('#0000FF');
-});
+    registerButtonListeners();
+};
 
+function appendTextArea() {
+    var gameArea = $('#game-area');
+    gameArea.append("<div class='row'>\
+        <div class='two-thids column textpanel'>\
+        <input type='text' class='form-control input-lg text-center'>\
+        </div></div>");
+};
+
+// Fixes buttons getting stuck down on mobile
+function registerButtonListeners() {
+    $(".btn").on("touchstart", function() {
+        $(this).removeClass("mobileHoverFix");
+    });
+
+    $(".btn").on("touchend", function() {
+        $(this).addClass("mobileHoverFix");
+    });
+
+    $("#submit-btn").click(function() {
+        appendTextArea();
+    });
+
+    $('#clear-btn').click(function() {
+        curPad.clear();
+    });
+
+    $('#c-black-btn').click(function() {
+        curPad.setLineColor('#000000');
+    });
+
+    $('#c-red-btn').click(function() {
+        curPad.setLineColor('#FF0000');
+    });
+
+    $('#c-green-btn').click(function() {
+        curPad.setLineColor('#00FF00');
+    });
+
+    $('#c-blue-btn').click(function() {
+        curPad.setLineColor('#0000FF');
+    });
+}
 },{"responsive-sketchpad":1}]},{},[2]);
--- src/js/index.js
+++ src/js/index.js
@@ -1,43 +1,107 @@
 var Sketchpad = require('responsive-sketchpad');
 
-// Initialize sketchpad
-var el = $('#sketchpad')[0];
-var pad = new Sketchpad(el, {
-    line: {
-        color: '#000000',
-        size: 5
-    }
+var padWidth;
+var padHeight;
+var curPad;
+
+
+$(window).on("load", function() {
+    // Initialize sketchpad
+    var el = $('#game-area')[0];
+    padWidth = $('#game-area').width();
+    padHeight = $(window).height() * 0.7;
+    console.log(padWidth + " " + padHeight);
+    appendTextArea();
+    appendSketchpad();
 });
 
-$(document).ready(function() {
+function appendSketchpad() {
+    var gameArea = $('#game-area');
+    var row = $('<div class="row"></div>');
+    var col1 = $('<div class="two-thirds column"></div>');
+    var panel = $('<div class="drawpanel" id="sketchpad"></div>');
 
-});
+    panel.appendTo(col1);
+
+    var buttons = $('<div class="one-third column">\
+          <div class="row">\
+            <div class="col-sm-1">\
+              <div class="btn btn-lg color-btn" id="c-black-btn">Blk</div>\
+            </div>\
+            <div class="col-sm-1">\
+              <div class="btn btn-lg color-btn" id="c-red-btn">Red</div>\
+            </div>\
+            <div class="col-sm-1">\
+              <div class="btn btn-lg color-btn" id="c-green-btn">Grn</div>\
+            </div>\
+            <div class="col-sm-1">\
+              <div class="btn btn-lg color-btn" id="c-blue-btn">Blu</div>\
+            </div>\
+            <div class="col-sm-4">\
+              <div class="btn btn-lg btn-block btn-danger action-btn" id="clear-btn">Clear</div>\
+            </div>\
+            <div class="col-sm-4">\
+              <div class="btn btn-lg btn-block btn-success action-btn" id="submit-btn">Submit</div>\
+            </div>\
+          </div>\
+        </div>');
+
+    curPad = new Sketchpad(panel[0], {
+        line: {
+            color: '#000000',
+            size: 5
+        },
+        width: padWidth,
+        height: padHeight
+    });
+
+    col1.appendTo(row);
+    buttons.appendTo(row);
+
+    row.appendTo(gameArea);
+
+    registerButtonListeners();
+};
+
+function appendTextArea() {
+    var gameArea = $('#game-area');
+    gameArea.append("<div class='row'>\
+        <div class='two-thids column textpanel'>\
+        <input type='text' class='form-control input-lg text-center'>\
+        </div></div>");
+};
 
 // Fixes buttons getting stuck down on mobile
-$(".btn").on("touchstart", function() {
-    $(this).removeClass("mobileHoverFix");
-});
+function registerButtonListeners() {
+    $(".btn").on("touchstart", function() {
+        $(this).removeClass("mobileHoverFix");
+    });
 
-$(".btn").on("touchend", function() {
-    $(this).addClass("mobileHoverFix");
-});
+    $(".btn").on("touchend", function() {
+        $(this).addClass("mobileHoverFix");
+    });
 
-$('#clear-btn').click(function() {
-    pad.clear();
-});
+    $("#submit-btn").click(function() {
+        appendTextArea();
+    });
 
-$('#c-black-btn').click(function() {
-    pad.setLineColor('#000000');
-});
+    $('#clear-btn').click(function() {
+        curPad.clear();
+    });
 
-$('#c-red-btn').click(function() {
-    pad.setLineColor('#FF0000');
-});
+    $('#c-black-btn').click(function() {
+        curPad.setLineColor('#000000');
+    });
 
-$('#c-green-btn').click(function() {
-    pad.setLineColor('#00FF00');
-});
+    $('#c-red-btn').click(function() {
+        curPad.setLineColor('#FF0000');
+    });
 
-$('#c-blue-btn').click(function() {
-    pad.setLineColor('#0000FF');
-});
+    $('#c-green-btn').click(function() {
+        curPad.setLineColor('#00FF00');
+    });
+
+    $('#c-blue-btn').click(function() {
+        curPad.setLineColor('#0000FF');
+    });
+}
